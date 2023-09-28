73ba1b78a574bb6e9fdaa814ddc35afa82e70ac6
Maxim Yakovlev <it.trainee@yandex.ru>
2023-09-28 16:23:21 +0300

    Added the @Body() decorator to accept any client params. Determined the Data Transfer Object schema.

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto): string {
     return 'This action adds a new cat';
   }
 
